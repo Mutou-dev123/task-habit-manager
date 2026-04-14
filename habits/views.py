5c48b161b0e3eba8f47369b458bed6a61be700f6
Yuga Kobayashi <yuga_kobayashi@ygcollege.jp>
2026-04-14 23:18:41 +0900

    add task app view and multiple modification

--- habits/views.py
+++ habits/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, "habits/home.html")
+    return render(request, "habits/habit_list.html")
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from .models import Task
+from django.shortcuts import get_object_or_404
 
 def task_list(request):
     tasks = Task.objects.all()
     return render(request, "tasks/task_list.html", {"tasks": tasks})
+
+def task_detail(request, pk):
+    task = get_object_or_404(Task, id=pk)
+    return render(request, "tasks/task_detail.html", {"task": task})
