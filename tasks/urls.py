56708e49d0e8445a17c5445660624c1bbae54b35
Yuga Kobayashi <yuga_kobayashi@ygcollege.jp>
2026-04-16 15:56:15 +0900

    Add edit and delete

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.task_list, name='task_list'),
     path('<int:pk>/', views.task_detail, name='task_detail'),
-    path("create/", views.task_create, name="task_create"),
+    path('create/', views.task_create, name='task_create'),
+    path('<int:pk>/edit/', views.task_update, name='task_update'),
+    path('<int:pk>/delete/', views.task_delete, name='task_delete'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 from .forms import TaskForm
-from django.shortcuts import get_object_or_404
 
 # タスク一覧
 def task_list(request):
@@ -24,3 +23,27 @@ def task_create(request):
         form = TaskForm()
 
     return render(request, "tasks/task_form.html", {"form": form})
+
+# タスク編集
+def task_update(request, pk):
+    task = get_object_or_404(Task, id=pk)
+
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect("task_list")
+    else:
+        form = TaskForm(instance=task)
+
+    return render(request, "tasks/task_form.html", {"form": form})
+
+# タスク削除
+def task_delete(request, pk):
+    task = get_object_or_404(Task, id=pk)
+
+    if request.method == "POST":
+        task.delete()
+        return redirect("task_list")
+    
+    return render(request, "tasks/task_confirm_delete.html", {"task": task})
