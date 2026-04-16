59d08bb22195ecb3ed946cf55fe0df7288cf4b5b
Yuga Kobayashi <yuga_kobayashi@ygcollege.jp>
2026-04-16 15:28:39 +0900

    Add task create, form and template

--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
 from django.shortcuts import get_object_or_404
 
+# タスク一覧
 def task_list(request):
     tasks = Task.objects.all()
     return render(request, "tasks/task_list.html", {"tasks": tasks})
 
+# タスク詳細
 def task_detail(request, pk):
     task = get_object_or_404(Task, id=pk)
     return render(request, "tasks/task_detail.html", {"task": task})
+
+# タスク作成
+def task_create(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("task_list")
+    else:
+        form = TaskForm()
+
+    return render(request, "tasks/task_form.html", {"form": form})
