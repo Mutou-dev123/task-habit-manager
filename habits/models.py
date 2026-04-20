a6a342a5626ffbc43f1168534c16f1eeba6619c4
Yuga Kobayashi <yuga_kobayashi@ygcollege.jp>
2026-04-21 00:15:07 +0900

    Add habit and habitlog models

--- habits/models.py
+++ habits/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Habit(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    start_date = models.DateField()
+    end_date = models.DateField(null=True, blank=True)
+    link = models.URLField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class HabitLog(models.Model):
+    habit = models.ForeignKey(Habit, on_delete=models.CASCADE)
+    date = models.DateField()
+
+    # 一日一回のみチェックを許可
+    class Meta:
+        unique_together = ("habit", "date")
