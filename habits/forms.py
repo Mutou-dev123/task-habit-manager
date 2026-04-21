23d6282b686bca6de47589d04a94281bf313e300
Yuga Kobayashi <yuga_kobayashi@ygcollege.jp>
2026-04-21 10:12:00 +0900

    Create habit form

---
+++ habits/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Habit
+
+# 習慣作成フォーム
+class HabitForm(forms.ModelForm):
+    class Meta:
+        model = Habit
+        fields = ["title", "description", "start_date", "end_date" "link"]
