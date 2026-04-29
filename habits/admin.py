3407876fbc1e817600733da0346f802e3e185fe5
Yuga Kobayashi <yuga_kobayashi@ygcollege.jp>
2026-04-30 00:55:53 +0900

    Implement HabitAdminForm for improved habit management in admin interface

--- habits/admin.py
+++ habits/admin.py
@@ -1,4 +1,18 @@
+from django import forms
 from django.contrib import admin
 from .models import Habit
 
-admin.site.register(Habit)
+class HabitAdminForm(forms.ModelForm):
+    weekdays = forms.MultipleChoiceField(
+        choices=Habit.WEEKDAY_CHOICES,
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+
+    def clean_weekdays(self):
+        return [int(w) for w in self.cleaned_data["weekdays"]]
+    
+class HabitAdmin(admin.ModelAdmin):
+    form = HabitAdminForm
+
+admin.site.register(Habit, HabitAdmin)
