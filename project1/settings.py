a118fe9144ec2c2b4563c4460aabf26e2ca572c5
Yuga Kobayashi <yuga_kobayashi@ygcollege.jp>
2026-06-16 22:29:30 +0900

    Update settings.py

--- project1/settings.py
+++ project1/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / "subdir".
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-q@9ns5la&+3ktbk+8q^)1i*ki3i(ztyejm8^p9&loy#oy__c(v"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = True
@@ -77,9 +81,13 @@ WSGI_APPLICATION = "project1.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
